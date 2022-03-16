e5e84f1ec798ccead0078b1c0412c1e4fa76de72
Shawn Andre Mbuvi <shawnmbuvi@gmail.com>
2022-03-16 14:49:55 +0300

    add git ignore

--- obj/Release/net5.0/win-x64/PubTmp/Out/wwwroot/js/jsvalidator.js
+++
@@ -1,8 +0,0 @@
-﻿'use-strict';
-
-$(".numericsonly").on("keypress keyup blur", function (event) {
-    $(this).val($(this).val().replace(/[^\d].+/, ""));
-    if ((event.which < 48 || event.which > 57)) {
-        event.preventDefault();
-    }
-});
